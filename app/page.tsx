eda9b87827f1012029d4d5b693524ee69f326892
Nanami Nakano <64841155+NanamiNakano@users.noreply.github.com>
2024-11-28 22:00:27 +0800

    fix: no user name after login

--- app/page.tsx
+++ app/page.tsx
@@ -84,52 +84,52 @@ function Page() {
           indexStore.setIndex(indexNumber)
         }
       }
+    }
 
-      if (typeof window !== "undefined") {
-        const access_token = localStorage.getItem("access_token")
-        if (access_token == "" || access_token == null) {
+    if (typeof window !== "undefined" && !userStore.user.name) {
+      const access_token = localStorage.getItem("access_token")
+      if (access_token == "" || access_token == null) {
+        dispatchToast(
+            <Toast>
+              <ToastTitle>Not logged in</ToastTitle>
+            </Toast>,
+            { intent: "error" },
+        )
+        router.push("/login")
+        return
+      }
+      checkUserMe(access_token).then(valid => {
+        if (!valid) {
+          localStorage.removeItem("access_token")
           dispatchToast(
               <Toast>
-                <ToastTitle>Not logged in</ToastTitle>
+                <ToastTitle>Session expired</ToastTitle>
               </Toast>,
               { intent: "error" },
           )
           router.push("/login")
-          return
         }
-        checkUserMe(access_token).then(valid => {
-          if (!valid) {
-            localStorage.removeItem("access_token")
-            dispatchToast(
-                <Toast>
-                  <ToastTitle>Session expired</ToastTitle>
-                </Toast>,
-                { intent: "error" },
-            )
-            router.push("/login")
-          }
-          return
-        })
-        userStore.fetch().catch(e => {
-          console.log(e)
-          dispatchToast(
-              <Toast>
-                <ToastTitle
-                    action={
-                      <ToastTrigger>
-                        <Button onClick={() => {
-                          router.refresh()
-                        }}>Reload app</Button>
-                      </ToastTrigger>
-                    }
-                >
-                  Fail loading user data
-                </ToastTitle>
-              </Toast>,
-              { intent: "error" },
-          )
-        })
-      }
+        return
+      })
+      userStore.fetch().catch(e => {
+        console.log(e)
+        dispatchToast(
+            <Toast>
+              <ToastTitle
+                  action={
+                    <ToastTrigger>
+                      <Button onClick={() => {
+                        router.refresh()
+                      }}>Reload app</Button>
+                    </ToastTrigger>
+                  }
+              >
+                Fail loading user data
+              </ToastTitle>
+            </Toast>,
+            { intent: "error" },
+        )
+      })
     }
   }, [])
 
