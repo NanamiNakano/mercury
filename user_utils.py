ef4610f5ecf9ef70972d8e11404ee1f8d38ea3e9
Nanami Nakano <64841155+NanamiNakano@users.noreply.github.com>
2025-01-04 18:12:28 +0800

    feat(user_utils)!: manage user purely from a CSV file

--- user_utils.py
+++ user_utils.py
@@ -1,3 +1,4 @@
+import csv
 import sqlite3
 import uuid
 import argon2
@@ -11,8 +12,9 @@ def generate_random_string(length=16):
     return ''.join(random.choice(characters) for _ in range(length))
 
 
-class DatabaseUtils:
-    def __init__(self, db_path):
+class UserUtils:
+    def __init__(self, db_path, csv_path):
+        self.csv_path = csv_path
         self.conn = sqlite3.connect(db_path)
         self.cursor = self.conn.cursor()
         self.ph = argon2.PasswordHasher(time_cost=2, memory_cost=19456, parallelism=1)
@@ -57,79 +59,54 @@ class DatabaseUtils:
         self.cursor.execute("DELETE FROM users WHERE user_id = ?", (user_id,))
         self.conn.commit()
 
+
+    def export(self):
+        csv_fp = open(self.csv_path, "w", newline="")
+        writer = csv.writer(csv_fp)
+        writer.writerow(["user_id", "user_name", "email", "password"])
+        rows = self.cursor.execute("SELECT * FROM users")
+        for user_id, user_name, email, _ in rows:
+            writer.writerow([user_id, user_name, email, ""]) # Passwords are hashed, so we don't export them
+        csv_fp.close()
+
+    def apply(self):
+        csv_fp = open(self.csv_path, newline="")
+        reader = csv.DictReader(csv_fp)
+        for row in reader:
+            user_id = row["user_id"]
+            password = row["password"]
+            user_name = row["user_name"]
+            email = row["email"]
+            if password is not None and password != "":
+                self.reset_user_password(user_id, password)
+            if user_name is not None and user_name != "":
+                self.change_user_name(user_id, user_name)
+            if email is not None and email != "":
+                self.change_user_email(user_id, email)
+        csv_fp.close()
+
     def close(self):
         self.conn.close()
 
 
 def main():
     main_parser = argparse.ArgumentParser(description="Manage users")
-    main_parser.add_argument("--sqlite_path", type=str, required=True, help="Path to the user SQLite database")
+    main_parser.add_argument("--sqlite_path", type=str, help="Path to the user SQLite database", default="./users.sqlite")
+    main_parser.add_argument("--csv", type=str, help="Path to the CSV file", default="./users.csv")
     user_commands_parser = main_parser.add_subparsers(dest="command", required=True)
 
-    new_user_parser = user_commands_parser.add_parser("new", help="Create a new user")
-    new_user_parser.add_argument("--user_name", type=str, required=True, help="Username of the new user")
-    new_user_parser.add_argument("--email", type=str, required=True, help="Email of the new user")
-    new_user_parser.add_argument("--password", type=str, help="Password of the new user")
-
-    delete_user_parser = user_commands_parser.add_parser("delete", help="Delete a user")
-    delete_user_parser.add_argument("--user_id", type=str, required=True, help="User ID to delete")
-
-    reset_password_parser = user_commands_parser.add_parser("reset_password", help="Reset a user's password")
-    reset_password_parser.add_argument("--user_id", type=str, required=True, help="User ID to reset the password")
-    reset_password_parser.add_argument("--new_password", type=str, help="New password for the user")
-
-    change_email_parser = user_commands_parser.add_parser("change_email", help="Change a user's email")
-    change_email_parser.add_argument("--user_id", type=str, required=True, help="User ID to change the email")
-    change_email_parser.add_argument("--new_email", type=str, required=True, help="New email for the user")
-
-    change_username_parser = user_commands_parser.add_parser("change_username", help="Change a user's username")
-    change_username_parser.add_argument("--user_id", type=str, required=True, help="User ID to change the username")
-    change_username_parser.add_argument("--new_username", type=str, required=True, help="New username for the user")
-
-    get_user_parser = user_commands_parser.add_parser("get", help="Get a user")
-    get_user_parser.add_argument("--user_id", type=str, help="User ID to get")
-    get_user_parser.add_argument("--email", type=str, help="Email to get")
+    user_commands_parser.add_parser("export", help="Export users to a CSV file")
+    user_commands_parser.add_parser("apply", help="Import users from a CSV file")
 
     args = main_parser.parse_args()
 
-    db_utils = DatabaseUtils(args.sqlite_path)
+    db_utils = UserUtils(args.sqlite_path, args.csv)
 
     match args.command:
-        case "new":
-            if args.password:
-                password = db_utils.new_user(args.user_name, args.email, args.password)
-            else:
-                password = db_utils.new_user(args.user_name, args.email)
-            print(f"New user created with password: {password}")
-        case "delete":
-            db_utils.delete_user(args.user_id)
-            print("User deleted")
-        case "reset_password":
-            if args.new_password:
-                new_password = db_utils.reset_user_password(args.user_id, args.new_password)
-            else:
-                new_password = db_utils.reset_user_password(args.user_id)
-            print(f"Password reset to: {new_password}")
-        case "change_email":
-            db_utils.change_user_email(args.user_id, args.new_email)
-            print("Email changed")
-        case "change_username":
-            db_utils.change_user_name(args.user_id, args.new_username)
-            print("Username changed")
-        case "get":
-            if args.user_id:
-                user = db_utils.get_user_by_id(args.user_id)
-            elif args.email:
-                user = db_utils.get_user_by_email(args.email)
-            else:
-                user = None
-
-            if user:
-                print(f"User ID: {user[0]}")
-                print(f"Username: {user[1]}")
-                print(f"Email: {user[2]}")
-            else:
-                print("User not found")
+        case "export":
+            db_utils.export()
+        case "apply":
+            db_utils.apply()
         case _:
             print("Invalid command")
 
