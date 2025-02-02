deaf86bda8ed858de9f1bca84b0840389e42958b
Nanami Nakano <64841155+NanamiNakano@users.noreply.github.com>
2025-02-02 19:58:13 +0800

    feat: scroll

--- components/ui/window.tsx
+++ components/ui/window.tsx
@@ -1,5 +1,5 @@
 import type { ReactElement } from "react"
-
+import { ScrollArea } from "@/components/ui/scroll-area"
 interface WindowProps {
   children: ReactElement
   name: string
@@ -11,7 +11,7 @@ export function Window({ children, name }: WindowProps) {
       <div className="flex items-center bg-slate-100 h-9 px-4">
         <h1>{name}</h1>
       </div>
-      <div className="p-4">{children}</div>
+      <ScrollArea className="p-4">{children}</ScrollArea>
     </div>
   )
 }
