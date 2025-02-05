142085a3ec187094ab1b91eb1f1f77777e41b5b8
Nanami Nakano <64841155+NanamiNakano@users.noreply.github.com>
2025-02-05 17:12:16 +0800

    feat: sidebar

---
+++ components/new/sidebar/index.tsx
@@ -0,0 +1,10 @@
+import { Window } from "@/components/ui/window"
+import EntryList from "./entry-list"
+
+export default function Sidebar() {
+  return (
+    <Window name="Annotations">
+      <EntryList />
+    </Window>
+  )
+}
