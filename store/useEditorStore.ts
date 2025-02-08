daac0c4ea78a9c2813bccc8bc1e3703dab3ceb47
Nanami Nakano <64841155+NanamiNakano@users.noreply.github.com>
2025-02-08 18:22:58 +0800

    feat: drop unused code

--- store/useEditorStore.ts
+++ store/useEditorStore.ts
@@ -1,26 +1,13 @@
-import type { LabelData, SectionResponse, SelectionRequest } from "../utils/types"
+import type { LabelData, SectionResponse } from "../utils/types"
 import { produce } from "immer"
 import { createTrackedSelector } from "react-tracked"
 import { create } from "zustand"
-import { getTaskHistory, selectText } from "../utils/request"
-import {
-  handleRequestError,
-  isRequestError,
-
-} from "../utils/types"
+import { getTaskHistory } from "../utils/request"
 
 interface EditorState {
-  sourceSelection: SelectionRequest
-  summarySelection: SelectionRequest
   initiator: "source" | "summary" | null
   serverSection: SectionResponse
-  editable: boolean
-  setSourceSelection: (start: number, end: number) => void
-  setSummarySelection: (start: number, end: number) => void
   clearAllSelection: () => void
-  clearSourceSelection: () => void
-  clearSummarySelection: () => void
-  fetchServerSection: (index: number) => Promise<void>
 
   history: LabelData[]
   viewingID: number | null
@@ -32,67 +19,15 @@ interface EditorState {
   setActiveList: (activeList: Record<number, boolean>) => void
 }
 
-export const useEditorStore = create<EditorState>()((set, get) => ({
-  sourceSelection: { start: -1, end: -1, from_summary: false },
-  summarySelection: { start: -1, end: -1, from_summary: true },
+export const useEditorStore = create<EditorState>()(set => ({
   initiator: null,
   serverSection: [],
   editable: true,
-  setSourceSelection: (start: number, end: number) => set(produce((state: EditorState) => {
-    state.sourceSelection = { start, end, from_summary: false }
-    if (state.initiator === null)
-      state.initiator = "source"
-  })),
-  setSummarySelection: (start: number, end: number) => set(produce((state: EditorState) => {
-    state.summarySelection = { start, end, from_summary: true }
-    if (state.initiator === null)
-      state.initiator = "summary"
-  })),
   clearAllSelection: () => set(produce((state: EditorState) => {
-    state.sourceSelection = { start: -1, end: -1, from_summary: false }
-    state.summarySelection = { start: -1, end: -1, from_summary: true }
     state.serverSection = []
     state.initiator = null
     window.getSelection()?.removeAllRanges()
   })),
-  clearSourceSelection: () => set(produce((state: EditorState) => {
-    state.sourceSelection = { start: -1, end: -1, from_summary: false }
-    if (state.initiator === "source") {
-      state.initiator = null
-      if (state.serverSection.length > 0)
-        state.serverSection = []
-    }
-  })),
-  clearSummarySelection: () => set(produce((state: EditorState) => {
-    state.summarySelection = { start: -1, end: -1, from_summary: true }
-    if (state.initiator === "summary") {
-      state.initiator = null
-      if (state.serverSection.length > 0)
-        state.serverSection = []
-    }
-  })),
-  fetchServerSection: async (index: number) => {
-    try {
-      if (get().initiator === "source") {
-        const response = await selectText(index, get().sourceSelection)
-        if (isRequestError(response)) {
-          handleRequestError(response)
-          return
-        }
-        set({ serverSection: response })
-      } else if (get().initiator === "summary") {
-        const response = await selectText(index, get().summarySelection)
-        if (isRequestError(response)) {
-          handleRequestError(response)
-          return
-        }
-        set({ serverSection: response })
-      }
-    } catch (e) {
-      console.warn(e)
-      throw e
-    }
-  },
 
   history: [],
   viewingID: null,
@@ -108,17 +43,7 @@ export const useEditorStore = create<EditorState>()((set, get) => ({
   },
   setHistory: (history: LabelData[]) => set({ history }),
   setViewing: (viewing: LabelData | null) => set(produce((state: EditorState) => {
-    if (viewing === null) {
-      state.editable = true
-      state.viewingID = null
-    } else {
-      state.sourceSelection.start = viewing.source_start
-      state.sourceSelection.end = viewing.source_end
-      state.summarySelection.start = viewing.summary_start
-      state.summarySelection.end = viewing.summary_end
-      state.editable = false
-      state.viewingID = viewing.record_id
-    }
+    state.viewingID = viewing?.record_id ?? null
   })),
   activeList: {},
   setActiveList: (activeList: Record<number, boolean>) => set({ activeList }),
