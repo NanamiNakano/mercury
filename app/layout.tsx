7dc3dcbb421642b5679a762a7093390ba72f130e
Nanami Nakano <64841155+NanamiNakano@users.noreply.github.com>
2025-02-08 22:47:32 +0800

    chore: remove unused packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 "use client"
+
 import { Toaster } from "@/components/ui/toaster"
-import "react-contexify/ReactContexify.css"
 import "@/app/globals.css"
 
 export default function RootLayout({ children }) {
